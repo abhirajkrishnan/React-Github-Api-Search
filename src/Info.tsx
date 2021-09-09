81829ad85b17e5e7bda203730f689f896517ffd4
abhiraj <abhirajkrishnan3@gmail.com>
2021-09-10 02:39:25 +0530

    nothing special

--- src/Info.tsx
+++ src/Info.tsx
@@ -4,6 +4,7 @@ import {RiGitRepositoryFill} from 'react-icons/ri'
 import {BsPeopleFill} from 'react-icons/bs'
 import {FiUserPlus} from 'react-icons/fi'
 import {GoGist} from 'react-icons/go'
+import data from './mockdata/mockUser'
 
 
 interface Props {
@@ -16,7 +17,7 @@ export default function Info({}: Props): ReactElement {
             <article className="info-box"> 
                 <span className="text-3xl text-center rounded-full text-pink-500 flex bg-pink-200 w-12 h-12 justify-center items-center ml-5"><RiGitRepositoryFill/></span>
                 <div>
-                    <h3 className="font-bold text-2xl">215 </h3>
+                    <h3 className="font-bold text-2xl">{data.public_repos} </h3>
                     <p>Repos</p>
                 </div>
             </article> 
@@ -24,7 +25,7 @@ export default function Info({}: Props): ReactElement {
             <article className="info-box"> 
                 <span className="text-3xl text-center rounded-full text-green-500 flex bg-green-200 w-12 h-12 justify-center items-center  ml-5"><BsPeopleFill/></span>
                 <div>
-                    <h3 className="font-bold text-2xl">215 </h3>
+                    <h3 className="font-bold text-2xl">{data.followers} </h3>
                     <p>Followers</p>
                 </div>
             </article> 
@@ -32,7 +33,7 @@ export default function Info({}: Props): ReactElement {
             <article className="info-box"> 
                 <span className="text-3xl text-center rounded-full text-indigo-500 flex bg-indigo-200 w-12 h-12 justify-center items-center  ml-5"><FiUserPlus/></span>
                 <div>
-                    <h3 className="font-bold text-2xl ">215 </h3>
+                    <h3 className="font-bold text-2xl ">{data.following} </h3>
                     <p>Following</p>
                 </div>
             </article> 
@@ -40,7 +41,7 @@ export default function Info({}: Props): ReactElement {
             <article className="info-box"> 
                 <span className="text-3xl text-center rounded-full text-yellow-500 flex bg-yellow-200 w-12 h-12 justify-center items-center  ml-5"><GoGist/></span>
                 <div>
-                    <h3 className="font-bold text-2xl">215 </h3>
+                    <h3 className="font-bold text-2xl">{data.public_gists} </h3>
                     <p>Gists</p>
                 </div>
             </article> 
