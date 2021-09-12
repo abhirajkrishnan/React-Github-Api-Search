3990b405d511c5e0a430f39ac238ed6b2274af01
abhiraj <abhirajkrishnan3@gmail.com>
2021-09-13 00:05:50 +0530

    routing

--- src/Error.tsx
+++ src/Error.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react'
+import { Link } from 'react-router-dom';
 
 interface Props {
     
@@ -13,13 +14,11 @@ function Error({}: Props): ReactElement {
           <div className="border-t border-gray-200 text-center pt-8">
             <h1 className="text-9xl font-bold text-purple-400">404</h1>
             <h1 className="text-6xl font-medium py-8">oops! Page not found</h1>
-            <p className="text-2xl pb-8 px-12 font-medium">Oops! The page you are looking for does not exist. It might have been moved or deleted.</p>
-            <button className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
-            HOME
+            <p className="text-2xl pb-8 px-12 font-medium">Oops! The page you are looking for does not exist.</p>
+            <Link to="/dashboard"><button className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
+            Back to Dashboard
             </button>
-            <button className="bg-gradient-to-r from-red-400 to-red-500 hover:from-red-500 hover:to-red-500 text-white font-semibold px-6 py-3 rounded-md">
-            Contact Us
-            </button>
+            </Link>
           </div>
         </div>
       </div>
