a232085fb1ec997cff696cb3708e734a96c9be3d
abhirajkrishnan <43444088+abhirajkrishnan@users.noreply.github.com>
2021-10-08 00:19:55 +0530

    trying something

--- src/FollowersCard.tsx
+++ src/FollowersCard.tsx
@@ -14,7 +14,9 @@ function FollowersCard(): ReactElement {
             {Follower.map(user=>{
                 return(
                     <article className="flex items-center p-2 hover:scale-105 hover:translate-x-4 ease-in-out duration-300 transform">
-                    <img src={user.avatar_url} alt="" className="w-10 h-10 lg:w-14 lg:h-14 rounded-full object-cover "/>
+                    <div className="w-10 h-10 lg:w-14 lg:h-14  ">
+                        <img src={user.avatar_url} alt="" className="rounded-full object-cover" />
+                    </div>    
                     <div className="pl-6 flex-col flex ">
                         <p className="text-sm lg:text-base font-bold">{user.login}</p>
                         <a href={user.html_url} target="_blank" rel="noreferrer" className="text-sm lg:text-base font-semibold text-gray-600">{user.html_url}</a>
